a6a59c9c7769e133849db7785d5c78be96fd866a
Richard Ayotte <rich@ayottesoftware.com>
2019-02-21 14:22:33 -0500

    ESLint with autofix
    The autofix touched all the files but the tests still pass!
    
    Code is now much more consistent and with some parts converted to ES5+ but there's still 1121 problems (111 errors, 1010 warnings) being reported eslint that I'm going to tackle next. They require manual changes so I'll be submitting those in separate PRs.

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+'use strict'
+
+module.exports = {
+	env: {
+		browser: false
+		, node: true
+	}
+	, extends: ['ayotte']
+}
--- index.js
+++ index.js
@@ -1,30 +1,53 @@
-var core = {
-	map : {},
-	abi : {},
-	literalTypes : ['string','bool','int','nat', 'bytes', 'mutez','timestamp','address','key','key_hash','signature','operation', 'unit','pkh'],
-	complexTypes : ['set','map','big_map','contract','list','option']
-};
+'use strict'
 
+const core = {
+	map: {}
+	, abi: {}
+	, literalTypes: [
+		'string'
+		, 'bool'
+		, 'int'
+		, 'nat'
+		, 'bytes'
+		, 'mutez'
+		, 'timestamp'
+		, 'address'
+		, 'key'
+		, 'key_hash'
+		, 'signature'
+		, 'operation'
+		, 'unit'
+		, 'pkh'
+	]
+	, complexTypes: [
+		'set'
+		, 'map'
+		, 'big_map'
+		, 'contract'
+		, 'list'
+		, 'option'
+	]
+}
 
-core.sha256 = require('js-sha256');
-core.helper = require("./lib/helper");
-core.parse = require("./lib/parse")(core);
-core.compile = require("./lib/compile")(core);
-core.abi = require("./lib/abi")(core);
-
+core.sha256 = require('js-sha256')
+core.helper = require('./lib/helper')
+core.parse = require('./lib/parse')(core)
+core.compile = require('./lib/compile')(core)
+core.abi = require('./lib/abi')(core)
 
 module.exports = {
-	compile: function(ml, config){
-		if (typeof config == 'undefined') config = {};
-		var _config = {
-			abi_format : (typeof config.abi_format != 'undefined' ? config.abi_format : 'compact'), //compact/full
-			ml_format : (typeof config.ml_format != 'undefined' ? config.ml_format : 'compact'), //compact, full, array
-			macros : (typeof config.macros != 'undefined' ? config.macros : true),
-		}		
+	compile(ml, config) {
+		if (typeof config == 'undefined') {
+			config = {}
+		}
+		const _config = {
+			abi_format: (typeof config.abi_format != 'undefined' ? config.abi_format : 'compact') // compact/full
+			, ml_format: (typeof config.ml_format != 'undefined' ? config.ml_format : 'compact') // compact, full, array
+			, macros: (typeof config.macros != 'undefined' ? config.macros : true)
+		}
 		return core.compile.script(ml, _config)
-	},
-	abi : core.abi,
-	version: "0.0.1b",
-	_core : core,
-};		
-
+	}
+	, abi: core.abi
+	, version: '0.0.1b'
+	, _core: core
+}
