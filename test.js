404318ea1ca5883dfc5e0f42e66548b2108a7191
Stephen Andrews <stephen@andrewstribe.co.nz>
2019-02-22 00:04:10 +1300

    Update tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var ficompiler = require("./index.js"),
 fs = require('fs'),
 path = require('path');
 
-var tests = ['bigmap','bool','bytes','input','key_hash','key','list','map','mutez','numbers','optional','set','signature','storage','string','timestamp', 'blank'];
+var tests = ['all', 'bigmap','bool','bytes','input','key_hash','key','list','map','mutez','numbers','optional','set','signature','storage','string','timestamp', 'blank'];
 
 for (var i = 0; i < tests.length; i++){
 	var t = tests[i];
@@ -11,7 +11,7 @@ for (var i = 0; i < tests.length; i++){
 			await expect(compileFile("test/"+t+"_test.fi")).resolves.toEqual(true);
 		});
 		it("Comparison test of " + t, async function(){
-			await expect(compileFile("test/"+t+"_test.fi")).resolves.toEqual(true);
+			await expect(compareFile("test/"+t+"_test.fi")).resolves.toEqual(true);
 		});
 	})(t);
 }
@@ -42,7 +42,7 @@ function compareFile(file){
 		fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
 			if (!err) {
 				try{
-					resolve(compiledFiles[file].ml == data);
+					resolve((compiledFiles[file].ml + ";") == (data).replace(/;}/g, '}'));
 				} catch(e){
 					reject(e);
 				}
